96bdfdd5c7b4faeff44a7ad29c93c83a83c442a1
Shiraz Ahmed <ahmedsha@iu.edu>
2019-10-08 10:18:54 -0400

    hi

--- models/recruiter.js
+++ models/recruiter.js
@@ -1,11 +1,29 @@
-// fullname, email, username, password
-
-module.exports = function (sequelize, DataTypes) { 
-
-    return sequelize.define('recruiter', {
-        fullName: DataTypes.STRING,
-        userName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        passwordhash: DataTypes.STRING
-    });
-};
+module.exports = (sequelize, DataTypes) => {
+    const Log = sequelize.define('log', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      comments: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      linkedin: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    })
+    return Log;
+  }
